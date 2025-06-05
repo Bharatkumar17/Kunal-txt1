e7d302cd46681a34332bf70ba948c96389ea8da9
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-06-05 17:00:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_NAME = "Moni Uploader 2"
-API_ID    = 24495656
-API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
+BOT_NAME = "Babu Uploader 2"
+API_ID    = 26729193
+API_HASH  = "a94598ef642481e35466292df95f251e"
+BOT_TOKEN = "7246658062:AAFihBmIU_oShvmhz1f-r8Rxu4dCt4Y950A"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
