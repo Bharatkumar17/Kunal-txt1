2e4ee53146916be5ada7992e76117dd6e759e56b
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-06-07 18:05:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BOT_NAME = "Babu Uploader 2"
 API_ID    = 26729193
 API_HASH  = "a94598ef642481e35466292df95f251e"
 BOT_TOKEN = "7246658062:AAFihBmIU_oShvmhz1f-r8Rxu4dCt4Y950A"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+MONGO_URI = "mongodb+srv://Uploaderbot:Uploaderbot@cluster0.mkxbrre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
